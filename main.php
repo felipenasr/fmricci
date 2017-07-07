414fb611c8a49f36a39dbd53572e706c780ab641
fericci <felipe.nascimento@seepix.com.br>
2017-07-07 18:25:09 -0300

    alinhando

--- main.php
+++ main.php
@@ -1,14 +1,26 @@
 <main class="mdl-layout__content">
-<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+
+
 	<form id="form" action="autenticacao.php" method="get">
 		
-		<input name="nome" class="mdl-textfield__input" type="text" id="nome-treinador" pattern="[a-zA-Z\sà-ùÀ-Ù,ä-ü,Ä-Ü]*">
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input name="nome" class="mdl-textfield__input" type="text" id="nome-treinador" pattern="[a-zA-Z\sà-ùÀ-Ù,ä-ü,Ä-Ü]*" />
 
-		<label class="mdl-textfield__label" for="nome-treinador">Nome do Treinador</label>
-		<span class="mdl-textfield__error">Não temos caracteres especiais no nosso nome!</span>
+			<label class="mdl-textfield__label" for="nome-treinador">Nome do Treinador</label>
+			<span class="mdl-textfield__error">Não temos caracteres especiais no nosso nome!</span>
+		</div>
+		
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input name="nome" class="mdl-textfield__input" type="text" id="nome-treinador" pattern="[0-9,/,-]+11">
 
-		<input type="submit" value="Submit">
+			<label class="mdl-textfield__label" for="nome-treinador">Data</label>
+			<span class="mdl-textfield__error">Não temos caracteres especiais no nosso nome!</span>
+		</div>
+
+		<div class="button-container">
+
+			<button type="submit" value="Submit" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary mdl-button--raised">Confirma</button>
+		</div>
 	
 	</form>
-</div>
 </main>
